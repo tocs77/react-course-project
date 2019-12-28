f884a78c47d470b1264692f6f6cb86619ab05df0
Ilya Golubkov <ilya.golubkov@gmail.com>
2019-12-28 13:34:39 +0600

    Add query params to pass ingredients

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -14,13 +14,13 @@ const checkoutSummary = (props) => {
             </div>
             <Button
                 btnType="Danger"
-                clicked>
+                clicked={props.checkoutCancelled}>
                 CANCEL
             </Button>
 
             <Button
             btnType="Success"
-            clicked>
+            clicked={props.checkoutContinued}>
                 CONTINUE
             </Button>
         </div>
