95290a414e8fd23a9ce6243c396e95793907c371
Ilya Golubkov <ilya.golubkov@gmail.com>
2019-12-30 08:48:35 +0600

    Added orders component and routing

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,18 +6,14 @@ import classes from './NavigationItems.module.css'
 
 const navigationItems = () =>(
     <ul className={classes.NavigationItems}>
-        <NavigationItem
-            link='/'
-            active
-        >
-            Burger Builder
-        </NavigationItem>
-        <NavigationItem
-        link='/'>
-            Checkout
-        </NavigationItem>
+        <NavigationItem link='/' exact>Burger Builder</NavigationItem>
+
+        <NavigationItem link='/orders'>Orders</NavigationItem>
+        
     </ul>
 
 )
 
 export default navigationItems
+
+//active
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import {Route, Switch} from 'react-router-dom'
 import Layout from '../hoc/Layout/Layout'
 import BurgerBuilder from './BurgerBuilder/BurgerBuilder'
 import Checkout from './CheckOut/CheckOut'
+import Orders from './Orders/Orders'
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout}/>
+            <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder}/>
           </Switch>
 
