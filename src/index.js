f04c095e4394e7e026200a24a49584cc77093c06
Ilya Golubkov <ilya.golubkov@gmail.com>
2020-01-09 13:14:19 +0600

    Added redux and init store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer'
 
+
+const store = createStore(reducer)
 
 const app = (
-    <BrowserRouter>
-        <App title="Burger shop"/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App title="Burger shop"/>
+        </BrowserRouter>  
+    </Provider>
+    
 );
 
 ReactDOM.render(app, document.getElementById('root'));
