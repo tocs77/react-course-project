ab47532d0b642e8f0d03e73f200c73bd86a971fa
Ilya Golubkov <ilya.golubkov@gmail.com>
2020-02-04 07:54:17 +0600

    Applied formatting

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react'
-import {Route, Switch} from 'react-router-dom'
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import Layout from '../hoc/Layout/Layout'
-import BurgerBuilder from './BurgerBuilder/BurgerBuilder'
-import Checkout from './CheckOut/CheckOut'
-import Orders from './Orders/Orders'
-
+import Layout from '../hoc/Layout/Layout';
+import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
+import Checkout from './CheckOut/CheckOut';
+import Orders from './Orders/Orders';
 
 class App extends Component {
-
   render() {
-
     return (
-      <div >
+      <div>
         <Layout>
           <Switch>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/orders" component={Orders} />
-            <Route path="/" exact component={BurgerBuilder}/>
+            <Route path='/checkout' component={Checkout} />
+            <Route path='/orders' component={Orders} />
+            <Route path='/' exact component={BurgerBuilder} />
           </Switch>
-
         </Layout>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import burgerBuilderReducer from './store/reducers/burgerBuilder'
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(burgerBuilderReducer, composeEnhancers(
-    applyMiddleware(thunk)
-))
+const store = createStore(
+  burgerBuilderReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App title="Burger shop"/>
-        </BrowserRouter>  
-    </Provider>
-    
+  <Provider store={store}>
+    <BrowserRouter>
+      <App title='Burger shop' />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
